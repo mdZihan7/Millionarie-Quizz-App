0dac923a48884318b168b3222bba13af55616d92
Zihan-ma1610630 <zihan.efran@gmail.com>
2023-05-13 19:44:44 +0300

    .

--- quiz/src/components/Quizzz.jsx
+++ quiz/src/components/Quizzz.jsx
@@ -1,12 +1,55 @@
-export default function Quizzz(){
+import { useEffect, useState } from "react"
+
+export default function Quizzz({
+    data, 
+    setStop, 
+    questionNumber, 
+    setQuestionNumber
+}){
+    const [question, setQuestion] = useState(null);
+    const [selectedAnswer, setSetlectedAnswer] = useState(null);
+    const [className, setClassName] = useState("answer");
+
+    useEffect(()=>{
+        setQuestion(data[questionNumber - 1])
+    },[data, questionNumber])
+    
+
+    const delay = (duration, callback) => {
+        setTimeout(() => {
+            callback();
+        }, duration);
+    };
+
+    const handleClick = (a) =>{
+        setSetlectedAnswer(a);
+        setClassName("answer active");
+        delay(3000, ()=>
+            setClassName(a.correct ? "answer correct" : "answer wrong")
+        );
+        delay(6000, ()=>
+           {
+                if(a.correct){
+                    setQuestionNumber(prev => prev +1);
+                    setSetlectedAnswer(null);
+                } else {
+                    setStop(true);
+                }
+           }
+        );
+    };
+
     return (
         <div className="quizzz">
-            <div className="question">Whats ?</div>
+            <div className="question">{question?.question}</div>
             <div className="answers">
-                <div className="answer">A</div>
-                <div className="answer">A</div>
-                <div className="answer">A</div>
-                <div className="answer">A</div>
+                {question?.answers.map((a)=> (
+                    <div className={setSetlectedAnswer === a? className : "answer"} 
+                    onClick={()=>handleClick(a)}
+                    >
+                        {a.text}
+                    </div>
+            ))}
             </div>
         </div>
     )
